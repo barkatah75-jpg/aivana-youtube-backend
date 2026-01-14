95b8247ecc7133cd1f70337b88db4f7dd48e8156
barkatah75-jpg <barkatah75@gmail.com>
2026-01-14 06:23:59 +0530

    AIVANA YouTube backend

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+  "name": "aivana-youtube-backend",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "googleapis": "^140.0.0",
+    "multer": "^1.4.5-lts.1",
+    "dotenv": "^16.4.5"
+  }
+}
